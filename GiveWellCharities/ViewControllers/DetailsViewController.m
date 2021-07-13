37baba3ad5d23e650b6f82fcfda9b348dee11863
Sophia Joy Wang <wangsj@fb.com>
2021-07-13 13:55:35 -0700

    Added Impact calculator to details view controller

--- GiveWellCharities/ViewControllers/DetailsViewController.m
+++ GiveWellCharities/ViewControllers/DetailsViewController.m
@@ -13,6 +13,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *summaryLabel;
 @property (weak, nonatomic) IBOutlet UILabel *missionLabel;
+@property (weak, nonatomic) IBOutlet UITextField *calculatorField;
+@property (weak, nonatomic) IBOutlet UILabel *quantityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *metricLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *modeControl;
+@property (weak, nonatomic) IBOutlet UILabel *minimumLabel;
 
 @end
 
@@ -24,6 +29,9 @@
     self.nameLabel.text = self.organization[@"organizationName"];
     self.summaryLabel.text = self.organization[@"summary"];
     self.missionLabel.text = self.organization[@"missionStatement"];
+    self.metricLabel.text = self.organization[@"metric"];
+    self.quantityLabel.text = @"0"; //starts at 0
+    self.minimumLabel.text = @""; //starts at an empty non-warning string
     
     //set organization logo image
     PFFileObject *logoPicture = self.organization[@"logo"];
@@ -32,6 +40,78 @@
     self.logoView.image = nil;
     [self.logoView setImageWithURL:logoURL];
 }
+
+- (IBAction)onTap:(id)sender {
+    //exit keypad
+    
+    //check if $5 minimum (for monthly or one-time) is met:
+    double amount = [self.calculatorField.text doubleValue];
+    double modeArray[] = {12, 1};
+    double modeMultiplier = modeArray[self.modeControl.selectedSegmentIndex];
+    double totalAmount = amount*modeMultiplier;
+    if (totalAmount >= 5){
+        NSLog(@"Exiting calculator");
+        [self.view endEditing:true];
+    } else {
+        NSLog(@"Donation Minimum not met");
+        self.minimumLabel.text = @"Value must be greater than or equal to 5.";
+        [self.view endEditing:true];
+        
+    }
+
+}
+
+- (IBAction)calculatingImpact:(id)sender {
+    double modeArray[] = {12, 1};
+    double modeMultiplier = modeArray[self.modeControl.selectedSegmentIndex];
+    [self calculator:modeMultiplier];
+}
+
+- (void)calculator:(double)modeMultiplier{
+    double amount = [self.calculatorField.text doubleValue];
+    NSLog(@"Editing changed");
+    double multiplier = [self.organization[@"multiplier"] doubleValue];
+    double monetaryAmount = amount*modeMultiplier;
+    double impact = amount*modeMultiplier*multiplier;
+    if ((impact - 0.5) < 0){
+        self.quantityLabel.text = @"0";
+        if (monetaryAmount < 5){
+            NSLog(@"Donation Minimum not met");
+            self.minimumLabel.text = @"Value must be greater than or equal to 5.";
+        } else {
+            NSLog(@"Donation minimum met");
+            self.minimumLabel.text = @""; //set as an empty non-warning string
+        }
+    } else {
+        double rounded = round(impact - 0.5);
+        NSLog(@"Amount calculated (unrounded): %.2f", impact);
+        NSLog(@"Amount calculated (rounded): %.0f", impact);
+        self.quantityLabel.text = [NSString stringWithFormat:@"%.0f", rounded];
+        
+        //check for minimum of $5 contribution
+        if (monetaryAmount < 5){
+            NSLog(@"Donation Minimum not met");
+            self.minimumLabel.text = @"Value must be greater than or equal to 5.";
+        } else {
+            NSLog(@"Donation minimum met");
+            self.minimumLabel.text = @""; //set as an empty non-warning string
+        }
+        
+    }
+    
+
+    
+    
+}
+
+- (IBAction)changedMode:(id)sender {
+    NSLog(@"Mode changed");
+    double modeArray[] = {12, 1};
+    double modeMultiplier = modeArray[self.modeControl.selectedSegmentIndex];
+    [self calculator:modeMultiplier];
+}
+
+
 
 /*
 #pragma mark - Navigation
